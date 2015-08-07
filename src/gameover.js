08793d2dc45f7b93d7aa83f2822c9cb9a548c8ca
Tomy Tang <yqtomy@hotmail.com>
2015-08-07 17:38:58 -0400

    Revert ed485b2..fb0e44d
    This rolls back to commit ed485b2378978cac03ebada095ef0e7a6501f732.

--- src/gameover.js
+++ src/gameover.js
@@ -68,10 +68,20 @@ var GameOverLayer = cc.Layer.extend({
         //////////////////////////////////
         //////////REASON/////////////
 
-        var reasonLabel = new cc.LabelBMFont("You missed twice in a row!", res.Junegull_BMFont);
+        var reasonLabel = new cc.LabelBMFont("reason", res.Junegull_BMFont);
         reasonLabel.x = size.width/2;
         reasonLabel.y = size.height/6*4;
-        reasonLabel.setScale(normalizescale);
+
+        if(unTouchedLossCounter  == 2){
+            reasonLabel.setString("Where did you go?");
+        }
+        else if (MISSLOSS == true){
+            reasonLabel.setString("You missed 2 in a row!")
+        }
+        else if (SLOWLOSS == true){
+            reasonLabel.setString("You slowed down too much!");
+        }
+        
         this.addChild(reasonLabel,5);
 
 
@@ -80,16 +90,16 @@ var GameOverLayer = cc.Layer.extend({
 
         ///////////////SCORES///////////////
         //score numbers
-        var highScoreNum = new cc.LabelBMFont(highScore, res.Junegull_BMFont);
+        highscore = ls.getItem("highscore");
+
+        var highScoreNum = new cc.LabelBMFont(highscore, res.Junegull_BMFont);
         highScoreNum.x = size.width/2;
         highScoreNum.y = size.height/6*2;
-        highScoreNum.setScale(normalizescale);
         this.addChild(highScoreNum,5);
 
         var yourScoreNum = new cc.LabelBMFont(""+currentScore,res.Junegull_BMFont);
         yourScoreNum.x = size.width/2;
         yourScoreNum.y = size.height/6*3;
-        highScoreNum.setScale(normalizescale);
         this.addChild(yourScoreNum,5);
 
         //score symbols
